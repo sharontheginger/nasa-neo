894ffffd8aa49507218ed6bd169af6df41a99906
Sharon <sharontheginger@me.com>
2018-01-16 12:51:57 -0800

    headline change

--- src/jumbo.js
+++ src/jumbo.js
@@ -7,7 +7,7 @@ class Jumbotron extends Component {
         <div>
             <div className="jumbotron">
                 <h1 className="display-3 jumbotext"><span id='headline'></span>Meteor Watch 2018!</h1>
-                <p className="lead insidetext">A list of meteors that may or may not kill us, as they are close to Earth.</p>
+                <p className="lead insidetext">A list of meteoroids that are close to earth</p>
                 <hr className="my-4"/>
                 <p className="lead">
                 <a className="btn btn-info btn-lg uglybutton" href="https://www.nasa.gov/" target="_blank"role="button">Learn more</a>
